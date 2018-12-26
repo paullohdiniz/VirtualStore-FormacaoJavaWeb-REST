67819db079e7b1c1ecc8423bf90dc017966e0500
Paulo Diniz <paulo.diniz@emphasysgroup.com.br>
2018-12-26 14:33:17 -0200

    Adição do projeto REST de controller

---
+++ virtual-store-controller/src/test/java/br/com/virtualstore/virtualstorecontroller/VirtualStoreControllerApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.virtualstore.virtualstorecontroller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualStoreControllerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
